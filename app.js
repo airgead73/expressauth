c40c1d9aaa3276af35e0ab62f8af6e86c29faad1
Dan Moneypenny <catullus1973@gmail.com>
2021-03-18 04:03:32 -0700

    init expressauth

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const { auth, requiresAuth } = require('express-openid-connect');
+
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    secret: process.env.SECRET
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'logged in': 'logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+
+  res.send(req.oidc.user);
+
+})
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+});
