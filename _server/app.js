355ddf6fdf4b22dccfb6611f8bb8859ee8ad3bb6
Dan Moneypenny <catullus1973@gmail.com>
2021-03-19 03:26:32 -0700

    add index.html

--- _server/app.js
+++ _server/app.js
@@ -1,9 +1,57 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const path = require('path');
+
 const { auth, requiresAuth } = require('express-openid-connect');
 const authConfig = require('./../_config/auth.config');
 
+/**
+ * INTIALIZE APP
+ */
+
+ const app = express();
+
+ /**
+ * SECURITY
+ */
+
+app.use(cors());
+app.use(helmet());
+
+/**
+ * MIDDLEWARE
+ */
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(auth(authConfig));
+app.use(express.static(path.join(__dirname, './../_PUBLIC')));
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+
+  console.log(req.headers);
+  next()
+
+});
+
+/**
+ * DEV MIDDLEWARE
+ */
+
+
+
+/**
+ * AUTHENTICATION
+ */
+
+
+
+/**
+ * LOAD ROUTES
+ */
 
 app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'logged in': 'logged out');
